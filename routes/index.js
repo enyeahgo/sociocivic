f66047013b95bd2223bc92729c5f8ea23569a38f
Inigo Orosco <enyeahgo@gmail.com>
2022-08-02 19:13:04 +0800

    New Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Socio-Civic'
+    title: 'Socio-Civic',
+    tagline: 'Collaboration. Commitment. Cooperation.'
   });
 });
 
